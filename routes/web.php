f2e325affc52d236c84b190b7cfd9df4674ce7af
AFS19 <iireco20@gmail.com>
2024-07-26 18:14:36 +0100

    add larave breeze for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,15 @@
 <?php
 
-use App\Livewire\CreateProduits;
-use App\Livewire\DeleteProduits;
-use App\Livewire\DetailsProduit;
-use App\Livewire\DetailsProduits;
-use App\Livewire\ListProduits;
-use App\Livewire\UpdateProduits;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+Route::view('/', 'welcome');
 
-// Route::group(["middleware" => "auth:api"], function () {
-//     Route::get('profile', [AuthController::class, 'profile'])->name("profile");
-//     Route::get('refresh', [AuthController::class, 'refreshToken'])->name("refreshToken");
-//     Route::get('logout', [AuthController::class, 'logout'])->name("logout");
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
 
-// produits routes
-Route::prefix('/produits')->group(function () {
-    Route::get("/list",  ListProduits::class)->name("list");
-    Route::get("/create", CreateProduits::class)->name("create");
-    Route::get("/{produitId}/details", DetailsProduits::class)->name("details");
-    Route::get("/{produitId/update",  UpdateProduits::class)->name("update");
-    Route::get("/{produitId}", DeleteProduits::class)->name("delete");
-});
-
-
-// panier et command
-// Route::get("/commande", )
+require __DIR__.'/auth.php';
